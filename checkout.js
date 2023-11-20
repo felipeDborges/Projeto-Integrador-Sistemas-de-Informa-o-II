57bfdf611637e8af4e9858a11274c5fca6e193e5
Felipe <feliped.borges@hotmail.com>
2023-11-20 20:38:32 -0300

    estilizando paginas com bootstrap

--- checkout.js
+++ checkout.js
@@ -1,4 +1,19 @@
-function onLoadFunction () {
-    const cupcakes = JSON.parse(sessionStorage.getItem ('cupcakes'));
-    console.log(cupcakes);
-};
+function onLoadFunction() {
+    const cupcakes = JSON.parse(sessionStorage.getItem('cupcakes'));
+    const cupcakesInfo = document.getElementById('cupcakesInfo');
+
+    if (cupcakes) {
+        let totalQuantity = 0;
+        let cupcakesHTML = '<h3 class="Cupcakes-selecionados">Cupcakes Selecionados:</h3><ul>';
+
+        cupcakes.forEach(cupcake => {
+            totalQuantity += cupcake.quantity;
+            cupcakesHTML += `<li>Name: ${cupcake.name}, Price: ${cupcake.price}, Quantity: ${cupcake.quantity}</li>`;
+        });
+
+        cupcakesHTML += '</ul>';
+        cupcakesInfo.innerHTML = cupcakesHTML;
+
+        document.getElementById('selectedQuantity').textContent = totalQuantity;
+    }
+}
