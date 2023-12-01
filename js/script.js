827e7a7078313307157e458168c88242adf91c48
Felipe D. Borges <feliped.borges@hotmail.com>
2023-12-01 14:30:56 -0300

    Consertando as paginas e dando os últimos retoques

--- js/script.js
+++ js/script.js
@@ -1,41 +1,12 @@
 let selectedCupcake = null;
 const loginForm = document.getElementById('loginForm');
-const signupButton = document.getElementById('signupButton');
-const checkoutFields = document.querySelectorAll('.checkout-finish input');
-const completePurchaseButton = document.querySelector('.completePurchase');
 
-function areFieldsValid() {
-    const creditCardValue = document.getElementById('creditCard').value;
-    const validThruValue = document.getElementById('validThru').value;
-    const securityCodeValue = document.getElementById('securityCode').value;
-    const addressValue = document.getElementById('address').value;
+loginForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Aqui você pode adicionar lógica de autenticação antes do redirecionamento, se necessário.
 
-    return (
-        creditCardValue.length === 19 && // Verifica se o campo 'creditCard' tem 19 caracteres
-        validThruValue.length === 7 && // Verifica se o campo 'validThru' tem 5 caracteres
-        securityCodeValue.length === 3 && // Verifica se o campo 'securityCode' tem 3 caracteres
-        addressValue.trim().length > 0 // Verifica se o campo 'address' não está vazio
-    );
-}
-
-function enableCompletePurchaseButton() {
-    completePurchaseButton.disabled = !areFieldsValid();
-}
-
-enableCompletePurchaseButton(); // Chama a função inicial para desabilitar o botão se os campos não estiverem preenchidos
-
-
-// Associa a função de validação ao evento 'input' em todos os campos de checkout
-checkoutFields.forEach(field => {
-    field.addEventListener('input', enableCompletePurchaseButton);
-});
-
-completePurchaseButton.addEventListener('click', function () {
-    if (areFieldsValid()) {
-        alert('Compra Finalizada!');
-    } else {
-        alert('Preencha todos os campos obrigatórios para continuar.');
-    }
+    // Redireciona para a página cupcakes.html
+    window.location.href = '../pages/cupcakes.html';
 });
 
 function goBack() {
--- js/signup.js
+++ js/signup.js
@@ -1,47 +1,36 @@
-document.getElementById('signupForm').addEventListener('submit', function (event) {
-    event.preventDefault();
+function applyPhoneFormatting() {
+    function formatPhone(value) {
+        let sanitizedValue = value.replace(/\D/g, ''); // Remove tudo exceto números
+        let formattedValue = '';
 
-    let email = document.getElementById('email').value;
-    let phone = document.getElementById('phone').value;
-    let password = document.getElementById('password').value;
+        if (sanitizedValue.length > 0) {
+            formattedValue = sanitizedValue.slice(0, 2);
 
-    let request = indexedDB.open('CadastroDB', 1); // Abre o banco de dados ou cria um novo se não existir
+            if (sanitizedValue.length >= 3) {
+                formattedValue += ' ' + sanitizedValue.slice(2, 3);
+            }
 
-    request.onerror = function (event) {
-        console.log("Erro ao abrir o banco de dados 'CadastroDB'.");
-    };
+            if (sanitizedValue.length >= 4) {
+                formattedValue += '.' + sanitizedValue.slice(3, 7);
+            }
 
-    request.onupgradeneeded = function (event) {
-        let db = event.target.result;
+            if (sanitizedValue.length >= 8) {
+                formattedValue += '-' + sanitizedValue.slice(7, 11);
+            }
+        }
 
-        let objectStore = db.createObjectStore("users", { keyPath: "email" }); // Cria um objeto store "users" no banco de dados
-        objectStore.createIndex("phone", "phone", { unique: false });
-        objectStore.createIndex("password", "password", { unique: false });
+        return formattedValue.trim();
+    }
 
-        console.log("Banco de dados 'CadastroDB' e objeto store 'users' criados com sucesso.");
-    };
+    document.getElementById('phone').addEventListener('blur', function (e) {
+        e.target.value = formatPhone(e.target.value);
+    });
+}
 
-    request.onsuccess = function (event) {
-        let db = event.target.result;
-        let transaction = db.transaction(["users"], "readwrite");
-        let objectStore = transaction.objectStore("users");
+// Chama a função para aplicar a formatação do telefone
+applyPhoneFormatting();
 
-        let newUser = {
-            email: email,
-            phone: phone,
-            password: password
-        };
 
-        let addRequest = objectStore.add(newUser); // Adiciona o novo usuário à IndexedDB
-
-        addRequest.onsuccess = function (event) {
-            console.log("Dados do usuário cadastrados com sucesso.");
-            alert("Dados do usuário cadastrados com sucesso.");
-        };
-
-        addRequest.onerror = function (event) {
-            console.log("Erro ao cadastrar dados do usuário.");
-            alert("Usuario já cadastrado.");
-        };
-    };
-});
+function goBack() {
+    window.history.back();
+}
